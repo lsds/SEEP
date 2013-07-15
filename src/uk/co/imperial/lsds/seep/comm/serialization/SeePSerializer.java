ca9b4d7c5bee2eca66b848a2ee6426c857fa9f99
Ra <r.castro-fernandez11@imperial.ac.uk>
2013-07-15 13:03:22 +0100

    Added license file and headers

--- src/uk/co/imperial/lsds/seep/comm/serialization/SeePSerializer.java
+++ src/uk/co/imperial/lsds/seep/comm/serialization/SeePSerializer.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Raul Castro Fernandez (Ra).
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *    Ra - Design and initial implementation
+ ******************************************************************************/
 package uk.co.imperial.lsds.seep.comm.serialization;
 
 /* SeePSerializer is a tentative generic solution that defines serialize and deserialize making serialization/deserialization solutions independent. However,
--- src/uk/co/imperial/lsds/seep/operator/OneTupleProcessing.java
+++ src/uk/co/imperial/lsds/seep/operator/OneTupleProcessing.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Raul Castro Fernandez (Ra).
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *    Ra - Design and initial implementation
+ ******************************************************************************/
 package uk.co.imperial.lsds.seep.operator;
 
 import uk.co.imperial.lsds.seep.comm.serialization.DataTuple;
