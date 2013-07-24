7354bb463c36226318e3dfae0dad1671f9b78a1f
Ra <rc3011@doc.ic.ac.uk>
2013-07-24 17:11:41 +0100

    Correct license and header per file.

--- src/uk/co/imperial/lsds/seep/comm/routing/StatefulRoutingImpl.java
+++ src/uk/co/imperial/lsds/seep/comm/routing/StatefulRoutingImpl.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
- * Copyright (c) 2013 Raul Castro Fernandez (Ra).
+ * Copyright (c) 2013 Imperial College London.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  * 
  * Contributors:
- *    Ra - Design and initial implementation
+ *     Raul Castro Fernandez - initial design and implementation
  ******************************************************************************/
 package uk.co.imperial.lsds.seep.comm.routing;
 
