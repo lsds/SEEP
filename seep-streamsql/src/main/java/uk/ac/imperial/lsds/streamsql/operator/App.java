f01e1c3e39e7cf993b3bf9de9c91f455d3901267
Matthias Weidlich <m.weidlich@imperial.ac.uk>
2014-04-22 17:26:37 +0100

    added module for streaming sql operators

---
+++ seep-streamsql/src/main/java/uk/ac/imperial/lsds/streamsql/operator/App.java
@@ -0,0 +1,27 @@
+package uk.ac.imperial.lsds.streamsql.operator;
+
+import net.sf.jsqlparser.JSQLParserException;
+import net.sf.jsqlparser.parser.CCJSqlParserUtil;
+import net.sf.jsqlparser.statement.Statement;
+
+
+public class App {
+    
+	public static void main(String[] args) {
+		System.out.println( "Test runner..." );
+       
+        try {
+        	
+			Statement stmt = CCJSqlParserUtil.parse("SELECT * FROM tab1");
+			stmt.accept(new SeepSQLVisitor());
+			
+			int i = 0;
+//			stmt.accept(statementVisitor)
+			
+			
+		} catch (JSQLParserException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    }
+}
