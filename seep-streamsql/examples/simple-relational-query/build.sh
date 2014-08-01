a693a560bcafad1fde78073a76dc090860c256d1
Alexandros Koliousis <a.koliousis@imperial.ac.uk>
2014-08-01 22:15:25 +0100

    Modified build and run scripts for example query.

--- seep-streamsql/examples/simple-relational-query/build.sh
+++ seep-streamsql/examples/simple-relational-query/build.sh
@@ -1,5 +1,21 @@
 #!/bin/sh
 
+SEEP="../../.."
 
-javac -cp .:./lib/seep-streamsql-0.0.1-SNAPSHOT.jar:./lib/seep-system-0.0.1-SNAPSHOT.jar ./src/*.java
+[ ! -d "lib" ] && mkdir lib/
+cp $SEEP/seep-system/target/seep-system-0.0.1-SNAPSHOT.jar lib/
+cp $SEEP/seep-streamsql/target/seep-streamsql-0.0.1-SNAPSHOT.jar lib/
 
+JCP="."
+JCP=${JCP}:lib/seep-streamsql-0.0.1-SNAPSHOT.jar
+JCP=${JCP}:lib/seep-system-0.0.1-SNAPSHOT.jar
+
+javac -cp $JCP -Xlint:unchecked ./src/*.java
+
+# Specific to GPU build
+[ -f CongestedSegRel.cl ] && rm -f CongestedSegRel.cl
+if [ "$1" == "gpu" ]; then
+	cp $SEEP/seep-system/src/main/java/uk/ac/imperial/lsds/seep/gpu/CongestedSegRel.cl .
+fi
+
+exit 0
--- seep-streamsql/examples/simple-relational-query/run.sh
+++ seep-streamsql/examples/simple-relational-query/run.sh
@@ -1,5 +1,48 @@
-#!/bin/sh
+#!/bin/bash
 
+DEFAULT_INPUT_FILE="datafile20seconds.dat"
+DEFAULT_EXEC_MODE="cpu"
+USAGE="usage: ./run.sh [cpu|gpu] [input filename]"
 
-java -cp .:./src/:./lib/seep-streamsql-0.0.1-SNAPSHOT.jar:./lib/seep-system-0.0.1-SNAPSHOT.jar LRBRunner datafile20seconds.dat
+# Classpath
+JCP="."
+JCP=${JCP}:src
+JCP=${JCP}:lib/seep-streamsql-0.0.1-SNAPSHOT.jar
+JCP=${JCP}:lib/seep-system-0.0.1-SNAPSHOT.jar
 
+# Java library path
+API="/mnt/data/cccad3/akolious/aparapi-read-only"
+JLP="${API}/com.amd.aparapi.jni/dist"
+
+OPTS="-server -XX:+UseConcMarkSweepGC -Xms8g -Xmx8g"
+
+if [ $# -gt 2 ]; then
+	echo $USAGE
+	exit 1
+fi
+
+# Set execution mode
+EXEC_MODE=$DEFAULT_EXEC_MODE
+[ $# -gt 0 ] && EXEC_MODE=$1
+echo "[DBG] mode is $EXEC_MODE"
+if [ "$EXEC_MODE" != "cpu" -a "$EXEC_MODE" != "gpu" ]; then
+	echo $USAGE
+	exit 1
+fi
+
+# Do not load aparapi library for CPU execution
+if [ "$EXEC_MODE" == "cpu" ]; then
+	JLP=""
+fi
+
+# Set input file
+INPUT_FILE=$DEFAULT_INPUT_FILE
+[ $# -gt 1 ] && INPUT_FILE=$2
+if [ ! -f $INPUT_FILE ]; then
+	echo "error: ${INPUT_FILE} does not exist"
+	exit 1
+fi
+
+java -Djava.library.path=$JLP $OPTS -cp $JCP LRBRunner $INPUT_FILE
+
+exit 0
