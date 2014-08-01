a91a6e08afd6c17d350ee2d3b477b6024d478c27
Alexandros Koliousis <a.koliousis@imperial.ac.uk>
2014-08-01 21:06:23 +0100

    Load Aparapi library upon build, like soot.

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
-mvn install:install-file -DgroupId=soot -DartifactId=soot-framework -Dversion=2.5.0 -Dpackaging=jar -Dfile=libs/soot/soot-framework/2.5.0/soot-2.5.0.jar 
+mvn install:install-file -DgroupId=soot -DartifactId=soot-framework -Dversion=2.5.0 -Dpackaging=jar -Dfile=libs/soot/soot-framework/2.5.0/soot-2.5.0.jar
+mvn install:install-file -DgroupId=aparapi -DartifactId=aparapi-framework -Dversion=0.0.1 -Dpackaging=jar -Dfile=libs/aparapi/aparapi-framework/0.0.1/aparapi-0.0.1.jar
+
 mvn clean compile assembly:single
