f3769c3e9cab290047023b21c30422e18ffd1e57
constantinos <cv1212@imperial.ac.uk>
2014-09-05 16:57:32 +0100

    Added basic implementation of the WebServer class

---
+++ seep-system/src/main/java/uk/ac/imperial/lsds/seep/interaction/WebServer.java
@@ -0,0 +1,39 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Imperial College London.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Constantinos Vryonides - initial design and implementation
+ ******************************************************************************/
+package uk.ac.imperial.lsds.seep.interactive;
+
+import org.eclipse.jetty.server.Server;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class WebServer {
+
+	final private static int PORT = 8686;
+	final private static Logger LOG = LoggerFactory.getLogger(Interactive.class);
+	final private static Server SERVER = new Server(PORT);
+
+	public WebServer() {
+
+	}
+
+	//Initialises the webserver and waits for incoming requests;
+	public void initWebServer() {
+		LOG.info("Starting webserver.");
+		try {
+			SERVER.start();
+			LOG.info("Webserver started successfully");
+			SERVER.join();
+		} catch (Exception e) {
+			LOG.warn("Webserver failed to start");
+		}
+	}
+}
