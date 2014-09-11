f08f065550debd6f8c540d1c211a515168ada1c9
dokeeffe <d.okeeffe@imperial.ac.uk>
2014-09-11 20:53:20 +0100

    Demo env setup scripts.

--- seep-system/examples/acita_demo_2015/core-emane/myservices/__init__.py
+++ seep-system/examples/acita_demo_2015/core-emane/myservices/__init__.py
@@ -4,4 +4,4 @@ Custom services that you define can be put in this directory.  Everything
 listed in __all__ is automatically loaded when you add this directory to the
 custom_services_dir = '/full/path/to/here' core.conf file option.
 """
-__all__ = ["sample","master","worker"]
+__all__ = ["sample","master","worker","android"]
