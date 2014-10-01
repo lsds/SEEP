34ed15a9b73d6610578fd42ccbacf37231e010bc
Dan O'Keeffe <dokeeffe@imperial.ac.uk>
2014-10-01 14:09:00 +0100

    Fix off by one error in policy table creation script that was breaking routing.

--- seep-system/examples/acita_demo_2015/core-emane/config/policy-tables-create.sh
+++ seep-system/examples/acita_demo_2015/core-emane/config/policy-tables-create.sh
@@ -4,7 +4,7 @@ echo 203 android-worker2 >> /etc/iproute2/rt_tables
 echo 204 android-worker3 >> /etc/iproute2/rt_tables 
 echo 205 android-worker4 >> /etc/iproute2/rt_tables 
 echo 206 android-worker5 >> /etc/iproute2/rt_tables 
-echo 206 android-worker6 >> /etc/iproute2/rt_tables 
+echo 207 android-worker6 >> /etc/iproute2/rt_tables 
 
 echo 211 android-master-core >> /etc/iproute2/rt_tables 
 echo 212 android-worker1-core >> /etc/iproute2/rt_tables 
@@ -12,4 +12,4 @@ echo 213 android-worker2-core >> /etc/iproute2/rt_tables
 echo 214 android-worker3-core >> /etc/iproute2/rt_tables 
 echo 215 android-worker4-core >> /etc/iproute2/rt_tables 
 echo 216 android-worker5-core >> /etc/iproute2/rt_tables  
-echo 216 android-worker6-core >> /etc/iproute2/rt_tables  
+echo 217 android-worker6-core >> /etc/iproute2/rt_tables  
