c822bc9f8dca03f8ba59738fcb726f6b8bb347c7
Ra <raul@contxt.in>
2014-10-14 02:02:38 +0200

    Basic migration of build system to gradle. Cleaning directories

--- bin/fabbash.sh
+++
@@ -1,9 +0,0 @@
-jar=$1
-properties=$2
-auth=$3
-machines=()
-
-for machine in "${machines[@]}";do
-	scp -i $auth $jar ubuntu@"${machine}:/home/ubuntu"
-	scp -i $auth $properties ubuntu@"${machine}:/home/ubuntu"
-done
--- build.sh
+++
@@ -1,2 +0,0 @@
-mvn install:install-file -DgroupId=soot -DartifactId=soot-framework -Dversion=2.5.0 -Dpackaging=jar -Dfile=libs/soot/soot-framework/2.5.0/soot-2.5.0.jar 
-mvn clean compile assembly:single
