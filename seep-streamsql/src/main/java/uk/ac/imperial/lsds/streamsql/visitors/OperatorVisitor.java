62a9deae3a57621712d37c7116bad3f254df6044
Alexandros Koliousis <a.koliousis@imperial.ac.uk>
2014-12-01 08:23:25 +0000

    Initial push of GPU operators

--- seep-streamsql/src/main/java/uk/ac/imperial/lsds/streamsql/visitors/OperatorVisitor.java
+++ seep-streamsql/src/main/java/uk/ac/imperial/lsds/streamsql/visitors/OperatorVisitor.java
@@ -1,15 +1,24 @@
 package uk.ac.imperial.lsds.streamsql.visitors;
 
+import uk.ac.imperial.lsds.streamsql.op.gpu.stateful.MicroAggregationKernel;
+import uk.ac.imperial.lsds.streamsql.op.gpu.stateless.ProjectionKernel;
+import uk.ac.imperial.lsds.streamsql.op.gpu.stateless.SelectionKernel;
+
 import uk.ac.imperial.lsds.streamsql.op.stateful.MicroAggregation;
 import uk.ac.imperial.lsds.streamsql.op.stateless.Projection;
 import uk.ac.imperial.lsds.streamsql.op.stateless.Selection;
 
 public interface OperatorVisitor {
 
-	public void visit(Projection projection);
+	public void visit (Projection projection);
 	
-	public void visit(Selection selection);
+	public void visit (Selection selection);
 
-	public void visit(MicroAggregation aggregation);
+	public void visit (MicroAggregation aggregation);
 
+	public void visit (ProjectionKernel projectionKernel);
+	
+	public void visit (SelectionKernel projectionKernel);
+	
+	public void visit (MicroAggregationKernel projectionKernel);
 }
