9d9d66ca8cfcbd773a7343501b04f9c6ba5a2eb8
Matthias Weidlich <m.weidlich@imperial.ac.uk>
2014-12-08 18:10:15 +0000

    minor

--- seep-system/src/main/java/uk/ac/imperial/lsds/seep/multi/ResultCollector.java
+++ seep-system/src/main/java/uk/ac/imperial/lsds/seep/multi/ResultCollector.java
@@ -1,29 +1,38 @@
 package uk.ac.imperial.lsds.seep.multi;
+
 public class ResultCollector {
-	
-	public static void free (ResultHandler handler, IQueryBuffer buffer, int taskid, int offset) {
+
+	public static void free(ResultHandler handler, IQueryBuffer buffer,
+			int taskid, int offset) {
 		int idx = taskid % handler.SLOTS;
 		int index;
 		int next;
 		try {
-			
-			while (! handler.slots.compareAndSet(idx, 1, 0))
-			{
-				/* System.err.println("warning: result collector blocked");
-				Thread.sleep(1L); */
+
+			while (!handler.slots.compareAndSet(idx, 1, 0)) {
+				/*
+				 * System.err.println("warning: result collector blocked");
+				 * Thread.sleep(1L);
+				 */
 				Thread.yield();
 			}
-		
-			handler.offsets.set (idx, offset);
-			index = handler.offsets.getAndSet (handler.next, -1);
-			while (index > 0)
-			{
-				buffer.free (index);
-				handler.slots.lazySet (handler.next, 1);
+
+			handler.offsets.set(idx, offset);
+			handler.results.set(idx, buffer);
+			index = handler.offsets.getAndSet(handler.next, -1);
+			while (index > 0) {
+				/*
+				 * Do the actual result forwarding here
+				 */
+
+				buffer.free(index);
+				handler.slots.lazySet(handler.next, 1);
 				next = (handler.next + 1) % handler.SLOTS;
-				index = handler.offsets.getAndSet (next, -1);
+				index = handler.offsets.getAndSet(next, -1);
 				handler.next = next;
 			}
-		} catch (Exception e) { e.printStackTrace(); } 
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
--- seep-system/src/main/java/uk/ac/imperial/lsds/seep/multi/ResultHandler.java
+++ seep-system/src/main/java/uk/ac/imperial/lsds/seep/multi/ResultHandler.java
@@ -1,17 +1,22 @@
 package uk.ac.imperial.lsds.seep.multi;
+
 import java.util.concurrent.atomic.AtomicIntegerArray;
+import java.util.concurrent.atomic.AtomicReferenceArray;
 
 public class ResultHandler {
-	
+
 	public final int SLOTS = Utils.TASKS;
-	
+
+	public AtomicReferenceArray<IQueryBuffer> results = new AtomicReferenceArray<>(
+			SLOTS);
+
 	public AtomicIntegerArray slots;
 	public AtomicIntegerArray offsets;
 	public int next;
-	
-	public ResultHandler () {
-		slots = new AtomicIntegerArray (SLOTS);
-		offsets = new AtomicIntegerArray (SLOTS);
+
+	public ResultHandler() {
+		slots = new AtomicIntegerArray(SLOTS);
+		offsets = new AtomicIntegerArray(SLOTS);
 		for (int i = 0; i < SLOTS; i++) {
 			slots.set(i, 1);
 			offsets.set(i, -1);
