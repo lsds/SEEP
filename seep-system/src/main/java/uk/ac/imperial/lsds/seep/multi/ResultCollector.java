09a234b17ac0f9bf2dac0ab8af72c4f19a22e51f
Alexandros Koliousis <a.koliousis@imperial.ac.uk>
2015-02-04 18:23:50 +0000

    Another result handler

--- seep-system/src/main/java/uk/ac/imperial/lsds/seep/multi/ResultCollector.java
+++ seep-system/src/main/java/uk/ac/imperial/lsds/seep/multi/ResultCollector.java
@@ -12,59 +12,114 @@ public class ResultCollector {
 		
 		int idx = taskid % handler.SLOTS;
 		int index;
-		int next;
+		int state;
+		
+//		System.out.println("Enter \t" + query.getId() +"\t"+ taskid+"\t"+ idx+"\t"+ freeOffset);
+
 		try {
 
-			while (!handler.slots.compareAndSet(idx, 1, 0)) {
-				/*
-				 * System.err.println("warning: result collector blocked");
-				 * Thread.sleep(1L);
+			while (!handler.slots.compareAndSet(idx, 0, 1)) {
+				/**/
+				// System.err.println("warning: result collector blocked");
+				/* Thread.sleep(1L);
 				 */
 				Thread.yield();
 			}
 
-			handler.offsets.set(idx, freeOffset);
-			handler.results.set(idx, buffer);
-			index = handler.offsets.getAndSet(handler.next, -1);
-			// int count = 0;
-			while (index != -1) {
+			handler.offsets[idx] = freeOffset;
+			handler.results[idx] = buffer;
+			
+			state = handler.slots.get(handler.next);
+			if (state == 1)
+				if (! handler.slots.compareAndSet(handler.next, state, 2))
+					return ;
+					// state = handler.slots.getAndSet(handler.next, 2);
+			
+			while (state == 1) {
+
+				IQueryBuffer tBuffer = handler.results[handler.next];
+				byte [] t = tBuffer.array();
+
 				/*
 				 * Do the actual result forwarding
 				 */
 				if (query.getDownstreamSubQuery() != null) {
-					IQueryBuffer tBuffer = handler.results.get(handler.next);
-					if (tBuffer == null)
-						System.err.println(String.format("error (1): query %d index %d handler.next %d", query.getId(), index, handler.next));
-					byte [] t = tBuffer.array();
-					if (t == null) {
-						System.err.println(String.format("error (2): query %d index %d handler.next %d", query.getId(), index, handler.next));
-						System.exit(1);
-					} else {
-						query.getDownstreamSubQuery().getTaskDispatcher()
-							.dispatch(t);
-//						System.err.println(String.format("[BDG] release buffer %s query %d index %d handler.next %d", 
-//								tBuffer, query.getId(), index, handler.next));
-						tBuffer.release();
+					query.getDownstreamSubQuery().getTaskDispatcher().dispatch(t);
+				}
+				// Here would go the forwarding to the distributed API
+
+				// Forwarded, let's release the buffer
+				tBuffer.release();
+
+				// Forwarded, let's free
+				index = handler.offsets[handler.next];
+				//System.out.println("Free \t" + handler.next +"\t"+ index);
+				if (index != Integer.MIN_VALUE) 
+					handler.freeBuffer.free(index);
+				
+				int previous = handler.next;
+				handler.next = (handler.next + 1) % handler.SLOTS;
+
+				state = handler.slots.get(handler.next);
+				if (state == 1) {
+					if (! handler.slots.compareAndSet(handler.next, state, 2)) {
+						handler.slots.set(previous, 0);
+						return ;
 					}
 				}
-				else {
-					/*
-					 * Push to distributed API
-					 */
-					IQueryBuffer tBuffer = handler.results.get(handler.next);
-					if (tBuffer == null)
-						System.err.println(String.format("error: query %d index %d handler.next %d", query.getId(), index, handler.next));
-					tBuffer.release();
+
+				if (! handler.slots.compareAndSet(previous, 2, 0)) {
+					System.err.println ("Fatal error.");
+					System.exit(1);
 				}
-				
-				// count ++;
-				if (index != Integer.MIN_VALUE)
-					handler.freeBuffer.free(index);
-				handler.slots.lazySet(handler.next, 1);
-				next = (handler.next + 1) % handler.SLOTS;
-				index = handler.offsets.getAndSet(next, -1);
-				handler.next = next;
 			}
+				
+//				/*
+//				 * Do the actual result forwarding
+//				 */
+//				if (query.getDownstreamSubQuery() != null) {
+//					IQueryBuffer tBuffer = handler.results.get(handler.next);
+//					if (tBuffer == null)
+//						System.err.println(String.format("error (1): query %d index %d handler.next %d", query.getId(), index, handler.next));
+//					byte [] t = tBuffer.array();
+//					if (t == null) {
+//						System.err.println(String.format("error (2): query %d index %d handler.next %d", query.getId(), index, handler.next));
+//						System.exit(1);
+//					} else {
+//						query.getDownstreamSubQuery().getTaskDispatcher()
+//							.dispatch(t);
+////						System.err.println(String.format("[BDG] release buffer %s query %d index %d handler.next %d", 
+////								tBuffer, query.getId(), index, handler.next));
+//						tBuffer.release();
+//					}
+//				}
+//				else {
+//					/*
+//					 * Push to distributed API
+//					 */
+//					IQueryBuffer tBuffer = handler.results.get(handler.next);
+//					if (tBuffer == null)
+//						System.err.println(String.format("error: query %d index %d handler.next %d", query.getId(), index, handler.next));
+//					tBuffer.release();
+//				}
+				
+//				// count ++;
+//				if (index != Integer.MIN_VALUE)
+//					handler.freeBuffer.free(index);
+//				
+//				int tmp = handler.next;
+//				state = (handler.next + 1) % handler.SLOTS;
+//				index = handler.offsets.getAndSet(state, -1);
+//				handler.next = state;
+//				handler.slots.set(tmp, 1);
+//			}
+//			
+//			handler.counter.decrementAndGet();
+//			
+//			if (! handler.lock.compareAndSet(1, 0)) {
+//				System.err.println ("Fatal error.");
+//				System.exit(1);
+//			}
 			// if (count > 0)
 			//	System.out.println (String.format("[DBG] task %4d count %10d", taskid, count));
 			
--- seep-system/src/main/java/uk/ac/imperial/lsds/seep/multi/ResultHandler.java
+++ seep-system/src/main/java/uk/ac/imperial/lsds/seep/multi/ResultHandler.java
@@ -1,5 +1,6 @@
 package uk.ac.imperial.lsds.seep.multi;
 
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicIntegerArray;
 import java.util.concurrent.atomic.AtomicReferenceArray;
 
@@ -9,21 +10,38 @@ public class ResultHandler {
 
 	public IQueryBuffer freeBuffer;
 	
-	public AtomicReferenceArray<IQueryBuffer> results = new AtomicReferenceArray<>(
-			SLOTS);
-
-	public AtomicIntegerArray slots;
-	public AtomicIntegerArray offsets;
 	public int next;
+	/*
+	 * Flags:
+	 *  0 - slot is FREE
+	 *  1 - slot is full, but nobody took it
+	 *  2 - slot is full, but somebody is working on it (do not touch!)
+	 */
+	public AtomicIntegerArray slots;
+
+	/*
+	 * Structures to hold the actual data
+	 */
+	public IQueryBuffer[] results = new IQueryBuffer[SLOTS];
+	public int[] offsets = new int[SLOTS]; 
+	
+	public AtomicInteger lock, counter;
 
 	public ResultHandler(IQueryBuffer freeBuffer) {
 		this.freeBuffer = freeBuffer;
 		slots = new AtomicIntegerArray(SLOTS);
-		offsets = new AtomicIntegerArray(SLOTS);
-		next = 0;
 		for (int i = 0; i < SLOTS; i++) {
-			slots.set(i, 1);
-			offsets.set(i, -1);
+			slots.set(i, 0);
+			offsets[i] = Integer.MIN_VALUE;
 		}
+		
+//		offsets = new AtomicIntegerArray(SLOTS);
+		next = 0;
+//		lock = new AtomicInteger(0);
+//		counter = new AtomicInteger(0);
+//		for (int i = 0; i < SLOTS; i++) {
+//			slots.set(i, 1);
+//			offsets.set(i, -1);
+//		}
 	}
 }
