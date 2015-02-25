9e6dc041d0f3f6cd8b345fb5fe14d75bbb35acc9
dokeeffe <d.okeeffe@imperial.ac.uk>
2015-02-25 17:56:59 +0000

    Switch to emane model.

--- seep-system/examples/acita_demo_2015/core-emane/run_sessions.py
+++ seep-system/examples/acita_demo_2015/core-emane/run_sessions.py
@@ -2,8 +2,10 @@
 
 import sys,os,time,re,argparse
 from core import pycore
+from core.constants import *
 from core.mobility import BasicRangeModel
 from core.mobility import Ns2ScriptedMobility 
+from core.emane.ieee80211abg import EmaneIeee80211abgModel
 
 
 svc_dir='/data/dev/seep-github/seep-system/examples/acita_demo_2015/core-emane/vldb/myservices'
@@ -57,7 +59,7 @@ def run_sessions(time_str, k, mob, sessions):
         print '*** Running session %d ***'%session
         run_session(time_str, k, mob, session)
 
-def run_session(time_str, k, mob, exp_session):
+def run_session(time_str, k, mob, exp_session, model=None):
     try:
         session = pycore.Session(cfg={'custom_services_dir':svc_dir, 'preservedir':'1'}, persistent=True)
         #session = pycore.Session(cfg={'custom_services_dir':svc_dir}, persistent=True)
@@ -66,34 +68,51 @@ def run_session(time_str, k, mob, exp_session):
             print 'Could not add to server'
             return
         """
+        if not model:
+            session.master = True
+            session.location.setrefgeo(47.57917,-122.13232,2.00000)
+            session.location.refscale = 150.0
+            session.cfg['emane_models'] = "RfPipe, Ieee80211abg, Bypass, AtdlOmni"
+            session.emane.loadmodels()
+
         #prefix = ipaddr.IPv4Prefix("10.0.0.0/32")
         #tmp.newnetif(net, ["%s/%s" % (prefix.addr(i), prefix.prefixlen)])
         # set increasing Z coordinates
-        wlan1 = session.addobj(cls = pycore.nodes.WlanNode, name="wlan1",objid=1,
-                verbose=True)
+            wlan1 = session.addobj(cls = pycore.nodes.EmaneNode, name = "wlan1", objid=1, verbose=True)
+        else:
+            wlan1 = session.addobj(cls = pycore.nodes.WlanNode, name="wlan1",objid=1, verbose=True)
 
-        print 'Basic Range Model default values: %s'%(str(BasicRangeModel.getdefaultvalues()))
-        wlan1.setmodel(BasicRangeModel, BasicRangeModel.getdefaultvalues())
-        wlan1.setposition(x=418.0,y=258.0)
+        wlan1.setposition(x=80,y=50)
+
+        if not model:
+            names = EmaneIeee80211abgModel.getnames()
+            values = list(EmaneIeee80211abgModel.getdefaultvalues())
+            print 'Emane Model default values: %s'%(str(list(EmaneIeee80211abgModel.getdefaultvalues())))
+            # TODO: change any of the EMANE 802.11 parameter values here
+            #values[ names.index('pathlossmode') ] = 'pathloss'
+            session.emane.setconfig(wlan1.objid, EmaneIeee80211abgModel._name, values)
+
+        else:
+            print 'Basic Range Model default values: %s'%(str(BasicRangeModel.getdefaultvalues()))
+            wlan1.setmodel(BasicRangeModel, BasicRangeModel.getdefaultvalues())
 
         services_str = "OLSR|IPForward"
 
         workers = []
         for i in range(2,8):
-            pos = gen_position(i)
+            pos = gen_linear_position(i)
             workers.append(create_node(i, session, "%s|MeanderWorker"%services_str, wlan1, pos)) 
         
-        master = create_node(8, session, "%s|MeanderMaster"%services_str, wlan1, gen_position(8))
+        master = create_node(8, session, "%s|MeanderMaster"%services_str, wlan1,
+                gen_linear_position(8))
 
 
+        """
         node_map = create_node_map(range(0,6), workers)
-        
         print 'Node map=%s'%node_map
-        #wlan1.setmodel(Ns2ScriptedMobility, tuple(map(lambda (key,val): str(val), mobility_params)))
         mobility_params[4] = ('map', node_map)
         session.mobility.setconfig_keyvalues(wlan1.objid, 'ns2script', mobility_params)
-
-        #wlan1.setmodel(Ns2ScriptedMobility, Ns2ScriptedMobility.getdefaultvalues())
+        """
 
         datacollect_hook = create_datacollect_hook(time_str, k, mob, exp_session) 
         session.sethook("hook:5","datacollect.sh",None,datacollect_hook)
@@ -117,8 +136,9 @@ def run_session(time_str, k, mob, exp_session):
 def create_node(i, session, services_str, wlan, pos, ip_offset=8):
     n = session.addobj(cls = pycore.nodes.CoreNode, name="n%d"%i)
     session.services.addservicestonode(n, "", services_str, verbose=False)
-    n.setposition(x=pos[0], y=pos[1])
     n.newnetif(net=wlan, addrlist=["10.0.0.%d/32"%(i+ip_offset)], ifindex=0)
+    n.cmd([SYSCTL_BIN, "net.ipv4.icmp_echo_ignore_broadcasts=0"])
+    n.setposition(x=pos[0], y=pos[1])
     return n
 
 def create_node_map(ns_nums, nodes):
@@ -128,10 +148,8 @@ def create_node_map(ns_nums, nodes):
     print 'nodes=%s'%str(nodes)
     return ",".join(map(lambda (ns_num, node) : "%d:%d"%(ns_num,node.objid), zip(ns_nums, nodes)))
 
-default_positions = {2 : (346.0,178.0),3:(515.0,160.0),4:(567.0,244.0),5:(558.0,317.0),6:(458.0,392.0),7:(349.0,359.0),8:(295.0,290.0)}
-def gen_position(i):
-    """TODO: Have different initial placement models etc."""
-    return default_positions[i] 
+def gen_linear_position(i):
+    return (50 * i, 100)
 
 def add_to_server(session):
     global server
@@ -156,6 +174,9 @@ def watch_meander_services(sessiondir, node_names):
 
         time.sleep(0.5)
 
+
+#def exists_mobility_trace(time_str, session):
+#    return os.path.isfile(
 
 if __name__ == "__main__" or __name__ == "__builtin__":
     parser = argparse.ArgumentParser(description='Run several meander experiments on CORE')
