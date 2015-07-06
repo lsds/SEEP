5b45c691a5958c0607ab9daadd555de49eb24ddd
Dan O'Keeffe <danokeeffe@gmail.com>
2015-07-06 16:04:36 +0100

    Start screen terminals automatically.

---
+++ screen.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+cd seep-system/examples/acita_demo_2015/core-emane
+echo `pwd`
+screen -d -m -S seep-ita
+
+
+for n in {1..4}; do
+	screen -S seep-ita -X screen $n
+done
+
+screen -S seep-ita -p 0 -X stuff 'cd ../../../..\n'
+screen -S seep-ita -p 1 -X stuff 'cd ..\n'
