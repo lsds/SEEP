4daff85cd5f4fa1331cbdc25d5536204653ea07e
Dan O'Keeffe <danokeeffe@gmail.com>
2015-09-02 13:05:56 +0000

    Fix up a few bugs in precise install script ; Add a trusty install script.

--- seep-system/examples/acita_demo_2015/core-emane/ubuntu_trusty_setup.sh
+++ seep-system/examples/acita_demo_2015/core-emane/ubuntu_trusty_setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 echo "Making directory."
 mkdir install
 cd install
@@ -6,12 +7,13 @@ cd install
 echo "Installing prerequisites."
 
 # First update and install prerequisite packages for CORE
-#sudo apt-get update
-#sudo apt-get install bash bridge-utils ebtables iproute libev-dev python tcl8.5 tk8.5 libtk-img
+sudo apt-get update
+sudo apt-get install bash bridge-utils ebtables iproute libev-dev python tcl8.5 tk8.5 libtk-img
 
-# Install EMANE prerequisites
-#sudo apt-get install libxml2 libprotobuf7 python-protobuf libpcap0.8 libpcre3 \
-#	libuuid1 libace-6.0.1 python-lxml python-setuptools
+echo "Installing Core from main repo."
+#Install EMANE prerequisites
+sudo apt-get install libxml2 libprotobuf8 python-protobuf libpcap0.8 libpcre3 \
+libuuid1 libace-6.0.3 python-lxml python-setuptools
 
 echo "Downloading quagga, CORE and EMANE."
 #Then download:
@@ -21,22 +23,22 @@ wget $QUAGGA_URL/quagga-0.99.21mr2.2/quagga-mr_0.99.21mr2.2_amd64.deb
 
 #Core
 export CORE_URL=http://downloads.pf.itd.nrl.navy.mil/core/packages
-wget "$CORE_URL/4.8/core-daemon_4.8-0ubuntu1_precise_amd64.deb"
-wget "$CORE_URL/4.8/core-gui_4.8-0ubuntu1_precise_all.deb"
+wget "$CORE_URL/4.8/core-daemon_4.8-0ubuntu1_trusty_amd64.deb"
+wget "$CORE_URL/4.8/core-gui_4.8-0ubuntu1_trusty_all.deb"
 
 #Emane
 export EMANE_URL=http://downloads.pf.itd.nrl.navy.mil/emane
-export EMANE_PKG=emane-0.9.2-release-1.ubuntu-12_04.amd64.tar.gz
+export EMANE_PKG=emane-0.9.2-release-1.ubuntu-14_04.amd64.tar.gz
 wget "$EMANE_URL/0.9.2-r1/$EMANE_PKG"
 tar -xzvf $EMANE_PKG 
 
 echo "Installing quagga, CORE and EMANE."
 sudo dpkg -i quagga-mr_0.99.21mr2.2_amd64.deb
 
-sudo dpkg -i core-daemon_4.8-0ubuntu1_precise_amd64.deb
-sudo dpkg -i core-gui_4.8-0ubuntu1_precise_all.deb
+sudo dpkg -i core-daemon_4.8-0ubuntu1_trusty_amd64.deb
+sudo dpkg -i core-gui_4.8-0ubuntu1_trusty_all.deb
 
-pushd emane-0.9.2-release-1/debs/ubuntu-12_04/amd64
+pushd emane-0.9.2-release-1/debs/ubuntu-14_04/amd64
 sudo dpkg -i emane*.deb
 popd
 
@@ -77,10 +79,10 @@ echo "Installing pip + python packages."
 sudo apt-get install python-pip
 
 #python pandas
-sudo pip install pandas
+#sudo pip install pandas
 
 #python matplotlib
-sudo pip install matplotlib
+#sudo pip install matplotlib
 
 #python utm
 sudo pip install utm
