1a0da4a23bbe9ea4c121bbe0926e08e1b535b379
Dan O'Keeffe <danokeeffe@gmail.com>
2015-10-26 14:34:56 +0000

    Add initial fixed routes service for grid topologies.

--- seep-system/examples/acita_demo_2015/core-emane/vldb/myservices/__init__.py
+++ seep-system/examples/acita_demo_2015/core-emane/vldb/myservices/__init__.py
@@ -4,4 +4,4 @@ Custom services that you define can be put in this directory.  Everything
 listed in __all__ is automatically loaded when you add this directory to the
 custom_services_dir = '/full/path/to/here' core.conf file option.
 """
-__all__ = ["sample","master","worker1","worker2","android","link_monitor","olsr_etx", "iperf_src", "iperf_sink"]
+__all__ = ["sample","master","worker1","worker2","android","link_monitor","olsr_etx", "iperf_src", "iperf_sink", "fixed_routes"]
