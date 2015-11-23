2c64ccfc27111c48d0bf111d2750573368265079
dokeeffe <d.okeeffe@imperial.ac.uk>
2015-11-23 17:15:05 +0000

    Allow to record and plot specific emane stats.

--- seep-system/examples/acita_demo_2015/core-emane/vldb/config/watch-emane-stats.sh
+++ seep-system/examples/acita_demo_2015/core-emane/vldb/config/watch-emane-stats.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
 while true ; do
+	required=`cat emane-required-stats.txt | tr '\n' ' '`
+	stats=`emanesh $1 get stat '*' mac $required`
 	IFS=$'\n'
-	for line in `emanesh $1 get stat '*' mac` ; do
+	for line in $stats ; do
 		stat=`echo $line | tr -s ' ' | cut -d ' ' -f 4`
 		echo `date +%s` $line >> $1-$stat-emane-stats.txt
 	done
 	unset IFS
-	sleep 5
+	sleep 5	
+#	IFS=$'\n'
+#	for line in `emanesh $1 get stat '*' mac` ; do
+#		stat=`echo $line | tr -s ' ' | cut -d ' ' -f 4`
+#		echo `date +%s` $line >> $1-$stat-emane-stats.txt
+#	done
+#	unset IFS
+#	sleep 5
 done
 
