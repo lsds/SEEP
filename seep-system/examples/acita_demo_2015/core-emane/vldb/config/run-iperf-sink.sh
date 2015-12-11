d44b72dd7b627ae6f6dea655722be55a53d0f477
dokeeffe <d.okeeffe@imperial.ac.uk>
2015-12-11 17:42:01 +0000

    Save various iperf configs.

--- seep-system/examples/acita_demo_2015/core-emane/vldb/config/run-iperf-sink.sh
+++ seep-system/examples/acita_demo_2015/core-emane/vldb/config/run-iperf-sink.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 #iperf -s -u -i 1
-iperf -s -i 10
+#iperf -s -i 10
+#iperf -s -i 10 -w 32728 -N -u
+#iperf -s -i 10 -w 32728 -N 
+iperf -s -i 10 -w 32728 -N 
+#iperf -u -s -i 2 -l 1024
--- seep-system/examples/acita_demo_2015/core-emane/vldb/config/run-iperf-src.sh
+++ seep-system/examples/acita_demo_2015/core-emane/vldb/config/run-iperf-src.sh
@@ -1,4 +1,9 @@
 #!/bin/bash 
 
 #iperf -c n3 -t 1000000 -i 1 -u -b 7m 
-iperf -c n3 -t 100 -i 10
+echo "iperf src starting sending to node $1"
+iperf -c $1 -t 10000 -i 10 -w 32728 -N
+#iperf -c $1 -t 10000 -i 10 -w 32728 -N -u -b 7M 
+
+#iperf -u -c $1 -b 40M -l 1024 -w 1024 -t 100000 -i 2
+echo "iperf src finished sending to node $1"
