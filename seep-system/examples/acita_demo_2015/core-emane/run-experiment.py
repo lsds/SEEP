521f6e087f543c9850e0d0e14a201eea8ca87393
dokeeffe <d.okeeffe@imperial.ac.uk>
2016-03-21 16:34:34 +0000

    Generalize experiment script to make it easier to vary grid size.

--- seep-system/examples/acita_demo_2015/core-emane/run-experiment.py
+++ seep-system/examples/acita_demo_2015/core-emane/run-experiment.py
@@ -18,7 +18,7 @@ latency_regex = re.compile('%s_lat=(\d+)'%(latency_percentile))
 #latency_percentile = 'max'
 #latency_regex = re.compile('max_lat=(\d+)')
 
-def main(ks,mobilities,nodes,sessions,params,plot_time_str=None):
+def main(ks,variables,sessions,params,plot_time_str=None):
 
     #script_dir = os.path.dirname(os.path.realpath(__file__))
     data_dir = '%s/log'%script_dir
@@ -29,21 +29,29 @@ def main(ks,mobilities,nodes,sessions,params,plot_time_str=None):
         time_str = plot_time_str
     else:
         time_str = time.strftime('%H-%M-%S-%a%d%m%y')
-        run_experiment(ks, mobilities, nodes, session_ids, params, time_str, data_dir )
+        #run_experiment(ks, mobilities, nodes, session_ids, params, time_str, data_dir )
+        run_experiment(ks, variables, session_ids, params, time_str, data_dir )
 
     if not params['iperf']:
-        record_dim_statistics(ks, mobilities, nodes, session_ids, time_str, data_dir, 'tput', get_tput)
-        record_dim_statistics(ks, mobilities, nodes, session_ids, time_str, data_dir, 'lat', get_latency)
+        #record_var_statistics(ks, mobilities, nodes, session_ids, time_str, data_dir, 'tput', get_tput)
+        #record_var_statistics(ks, mobilities, nodes, session_ids, time_str, data_dir, 'lat', get_latency)
+        record_var_statistics(ks, variables, session_ids, time_str, data_dir, 'tput', get_tput)
+        record_var_statistics(ks, variables, session_ids, time_str, data_dir, 'lat', get_latency)
 
-        if len(mobilities) > 1:
+        
+        if len(variables['mobility']) > 1:
             for p in ['tput_vs_mobility', 'median_tput_vs_mobility', 
                 'latency_vs_mobility', 'tput_vs_mobility_stddev', 
                 'latency_vs_mobility_stddev', 'rel_tput_vs_mobility_stddev',
                 'rel_latency_vs_mobility_stddev', 'tput_vs_netsize_stddev']:
                 plot(p, time_str, script_dir, data_dir)
-        if len(nodes) > 1:
+        elif len(variables['nodes']) > 1:
             for p in ['tput_vs_nodes_stddev', 'latency_vs_nodes_stddev', 
                     'rel_tput_vs_nodes_stddev', 'rel_latency_vs_nodes_stddev']:
+                plot(p, time_str, script_dir, data_dir)
+        elif len(variables['dimension']) > 1:
+            for p in ['tput_vs_dimension_stddev', 'latency_vs_dimension_stddev', 
+                    'rel_tput_vs_dimension_stddev', 'rel_latency_vs_dimension_stddev']:
                 plot(p, time_str, script_dir, data_dir)
         else:
             plot('m1_tput_vs_mobility_stddev', time_str, script_dir, data_dir)
@@ -56,8 +64,8 @@ def main(ks,mobilities,nodes,sessions,params,plot_time_str=None):
 	# Do any plots that summarize all sessions for fixed k and mob.
 
         for k in ks:
-            if len(nodes) == 1:
-                for mob in mobilities:
+            if len(variables['nodes']) <= 1 and len(variables['dimension']) <= 1:
+                for mob in variables['mobility']:
                     plot_fixed_kmob('cum_lat_fixed_kmob', k, mob, sessions, time_str, script_dir, data_dir, params)
                     plot_fixed_kmob('cum_raw_lat_fixed_kmob', k, mob, sessions, time_str, script_dir, data_dir, params)
 
@@ -87,8 +95,8 @@ def get_daemon_server():
     global server
     return server
 
-def get_session_dir(k, dim, dim_suffix, session, time_str, data_dir):
-    return '%s/%s/%dk/%.2f%s/%ds'%(data_dir, time_str, k, dim, dim_suffix, session)
+def get_session_dir(k, var, var_suffix, session, time_str, data_dir):
+    return '%s/%s/%dk/%.2f%s/%ds'%(data_dir, time_str, k, var, var_suffix, session)
 
 def create_exp_dirs(ks, mobilities, sessions, time_str, data_dir):
     root_exp_dir = '%s/%s'%(data_dir,time_str)
@@ -101,96 +109,108 @@ def create_exp_dirs(ks, mobilities, sessions, time_str, data_dir):
             for session in range(0, sessions):
                 os.mkdir('%s/%ds'%(mob_dir, session))
 
-def run_experiment(ks, mobilities, nodes, sessions, params, time_str, data_dir):
+def run_experiment(ks, variables, sessions, params, time_str, data_dir):
     #TODO: Not using data dir here!
     for k in ks:
-        if len(nodes) == 1:
-            for mob in mobilities:
-                run_sessions(time_str, k, mob, nodes[0], 'm', sessions, params)
+        if len(variables['nodes']) > 1:
+            for n in variables['nodes']:
+                run_sessions(time_str, k, variables['mobility'][0], n, 'n', sessions, params)
+        elif len(variables['dimension']) > 1:
+            for dim in variables['dimension']:
+                params['x'] = dim
+                params['y'] = dim
+                run_sessions(time_str, k, variables['mobility'][0], variables['nodes'][0], 'd', sessions, params)
         else:
-            for n in nodes:
-                run_sessions(time_str, k, mobilities[0], n, 'n', sessions, params)
+            for mob in variables['mobility']:
+                run_sessions(time_str, k, mob, variables['nodes'][0], 'm', sessions, params)
                     
-def record_dim_statistics(ks, mobilities, nodes, sessions, time_str, data_dir, metric_suffix, get_metric_fn):
+def record_var_statistics(ks, variables, sessions, time_str, data_dir, metric_suffix, get_metric_fn):
     raw_vals = {}
     all_loglines = []
 
-    dims = nodes if len(nodes) > 1 else mobilities
-    dim_suffix = 'n' if len(nodes) > 1 else 'm'
+    if len(variables['nodes']) > 1:
+        var_vals = variables['nodes']
+        var_suffix = 'n'
+    elif len(variables['dimension']) > 1:
+        var_vals = variables['dimension']
+        var_suffix = 'd'
+    else: 
+        var_vals = variables['mobility']
+        var_suffix = 'm'
     
     for k in ks:
         raw_vals[k] = {}
-        for (i_dim, dim) in enumerate(dims):
-            writeHeader = i_dim == 0
-            metrics = get_metrics(k, dim, dim_suffix, sessions, time_str, data_dir, get_metric_fn)
+        for (i_var, var) in enumerate(var_vals):
+            writeHeader = i_var == 0
+            metrics = get_metrics(k, var, var_suffix, sessions, time_str, data_dir, get_metric_fn)
 
-            #First record any cumulative stats for fixed kdim
-            record_fixed_kdim_statistics(k, dim, dim_suffix, metrics.values(), time_str, data_dir, metric_suffix)
+            #First record any cumulative stats for fixed kvar
+            record_fixed_kvar_statistics(k, var, var_suffix, metrics.values(), time_str, data_dir, metric_suffix)
 
-	    #Next record cumulative stats for raw  latencies for fixed kdim (if recording latency stats)
-	    if metric_suffix == 'lat': 
-                raw_latencies = [lat for session_lats in get_metrics(k, dim, dim_suffix, sessions, time_str, data_dir, get_raw_latencies).values() for lat in session_lats]
-                record_fixed_kdim_statistics(k, dim, dim_suffix, raw_latencies, time_str, data_dir, 'raw-lat')
+            #Next record cumulative stats for raw  latencies for fixed kvar (if recording latency stats)
+            if metric_suffix == 'lat': 
+                raw_latencies = [lat for session_lats in get_metrics(k, var, var_suffix, sessions, time_str, data_dir, get_raw_latencies).values() for lat in session_lats]
+                record_fixed_kvar_statistics(k, var, var_suffix, raw_latencies, time_str, data_dir, 'raw-lat')
 
-            #Now record any aggregate stats across all kdim 
-            raw_vals[k][dim] = metrics 
+            #Now record any aggregate stats across all kvar 
+            raw_vals[k][var] = metrics 
             meanVal,stdDevVal,maxVal,minVal,medianVal,lqVal,uqVal = compute_stats(metrics.values())  
 
-            # record stats vs dimility 
-            with open('%s/%s/%dk-%s.data'%(data_dir,time_str,k, metric_suffix),'w' if writeHeader else 'a') as rx_vs_dim_plotdata:
+            # record stats vs varility 
+            with open('%s/%s/%dk-%s.data'%(data_dir,time_str,k, metric_suffix),'w' if writeHeader else 'a') as rx_vs_var_plotdata:
                 if writeHeader: 
-                    rx_vs_dim_plotdata.write('#k=%d\n'%k)
-                    rx_vs_dim_plotdata.write('#dim mean k stdDev max min med lq uq\n')
-                logline = '%.4f %.1f %d %.1f %.1f %.1f %.1f %.1f %.1f\n'%(dim,meanVal, k, stdDevVal, maxVal, minVal, medianVal, lqVal, uqVal)
-                rx_vs_dim_plotdata.write(logline)
+                    rx_vs_var_plotdata.write('#k=%d\n'%k)
+                    rx_vs_var_plotdata.write('#var mean k stdDev max min med lq uq\n')
+                logline = '%.4f %.1f %d %.1f %.1f %.1f %.1f %.1f %.1f\n'%(var,meanVal, k, stdDevVal, maxVal, minVal, medianVal, lqVal, uqVal)
+                rx_vs_var_plotdata.write(logline)
                 all_loglines.append(logline)
 
     # Write a joint log file too in case we want to plot a histogram
-    with open('%s/%s/all-k-%s.data'%(data_dir,time_str,metric_suffix),'w') as all_rx_vs_dim_plotdata:
+    with open('%s/%s/all-k-%s.data'%(data_dir,time_str,metric_suffix),'w') as all_rx_vs_var_plotdata:
         for line in all_loglines:
-           all_rx_vs_dim_plotdata.write(line) 
+           all_rx_vs_var_plotdata.write(line) 
 
 	#TODO Do relative weights with raw_vals.
     if 1 in ks:
         all_loglines = []
         relative_raw_vals = compute_relative_raw_vals(raw_vals)
         for k in ks:
-            for (i_dim, dim) in enumerate(dims):
-                writeHeader = i_dim == 0
-                metrics = relative_raw_vals[k][dim]
+            for (i_var, var) in enumerate(var_vals):
+                writeHeader = i_var == 0
+                metrics = relative_raw_vals[k][var]
                 meanVal,stdDevVal,maxVal,minVal,medianVal,lqVal,uqVal = compute_stats(metrics.values())  
 
-                #record relative stats vs dimility
-                with open('%s/%s/%dk-rel-%s.data'%(data_dir,time_str,k,metric_suffix), 'w' if writeHeader else 'a') as rel_rx_vs_dim_plotdata:	
+                #record relative stats vs varility
+                with open('%s/%s/%dk-rel-%s.data'%(data_dir,time_str,k,metric_suffix), 'w' if writeHeader else 'a') as rel_rx_vs_var_plotdata:	
                     if writeHeader:
-                        rel_rx_vs_dim_plotdata.write('#k=%d\n'%k)
-                        rel_rx_vs_dim_plotdata.write('#dim mean k stdDev max min med lq uq\n')
-                    logline =  '%.4f %.1f %d %.1f %.1f %.1f %.1f %.1f %.1f\n'%(dim,meanVal, k, stdDevVal, maxVal, minVal, medianVal, lqVal, uqVal)
-                    rel_rx_vs_dim_plotdata.write(logline)
+                        rel_rx_vs_var_plotdata.write('#k=%d\n'%k)
+                        rel_rx_vs_var_plotdata.write('#var mean k stdDev max min med lq uq\n')
+                    logline =  '%.4f %.1f %d %.1f %.1f %.1f %.1f %.1f %.1f\n'%(var,meanVal, k, stdDevVal, maxVal, minVal, medianVal, lqVal, uqVal)
+                    rel_rx_vs_var_plotdata.write(logline)
                     all_loglines.append(logline)
 
         # Write a joint log file too in case we want to plot a histogram
-        with open('%s/%s/all-k-rel-%s.data'%(data_dir,time_str,metric_suffix),'w') as all_rel_rx_vs_dim_plotdata:
+        with open('%s/%s/all-k-rel-%s.data'%(data_dir,time_str,metric_suffix),'w') as all_rel_rx_vs_var_plotdata:
             for line in all_loglines:
-               all_rel_rx_vs_dim_plotdata.write(line) 
+               all_rel_rx_vs_var_plotdata.write(line) 
 
-def record_fixed_kdim_statistics(k, dim, dim_suffix, values, time_str, data_dir, metric_suffix):
+def record_fixed_kvar_statistics(k, var, var_suffix, values, time_str, data_dir, metric_suffix):
 
     #First sort the metrics in ascending order
     #Then get the total number of metrics
     #Then get the x increment per value 
     cum_distribution = compute_cumulative_percentiles(values) 
 
-    with open('%s/%s/%dk/%.2f%s/cum-%s.data'%(data_dir,time_str,k,dim,dim_suffix,metric_suffix),'w') as cum_fixed_kdim_plotdata:
-	cum_fixed_kdim_plotdata.write('#p %s\n'%metric_suffix)
+    with open('%s/%s/%dk/%.2f%s/cum-%s.data'%(data_dir,time_str,k,var,var_suffix,metric_suffix),'w') as cum_fixed_kvar_plotdata:
+	cum_fixed_kvar_plotdata.write('#p %s\n'%metric_suffix)
 	for (p, value) in cum_distribution: 
             logline = '%.2f %d\n'%(p,value)
-            cum_fixed_kdim_plotdata.write(logline)
+            cum_fixed_kvar_plotdata.write(logline)
 
-def get_metrics(k, dim, dim_suffix, sessions, time_str, data_dir, get_metric_fn):
+def get_metrics(k, var, var_suffix, sessions, time_str, data_dir, get_metric_fn):
     metrics = {} 
     for session in sessions:
-        logdir = '%s'%(get_session_dir(k,dim,dim_suffix, session,time_str,data_dir))
+        logdir = '%s'%(get_session_dir(k,var,var_suffix, session,time_str,data_dir))
         metric = get_metric_fn(logdir)
         metrics[session] = metric 
 
@@ -269,11 +289,11 @@ if __name__ == "__main__" or __name__ == "__builtin__":
     parser = argparse.ArgumentParser(description='Run simulations.')
     parser.add_argument('--ks', dest='ks', default='1,2,3,5', help='replication factors [1,2,3,5]')
     parser.add_argument('--h', dest='h', default='2', help='chain length (2)')
-    parser.add_argument('--x', dest='x', default='1200', help='Grid x dimension (1200)')
-    parser.add_argument('--y', dest='y', default='1200', help='Grid y dimension (1200)')
+    #parser.add_argument('--x', dest='x', default='1200', help='Grid x dimension (1200)')
+    #parser.add_argument('--y', dest='y', default='1200', help='Grid y dimension (1200)')
+    parser.add_argument('--dimension', dest='dimension', default='1200', help='Grid dimension (1200)')
     parser.add_argument('--query', dest='query', default='chain', help='query type: (chain), join, debsgc, fr, frshard, nameassist')
     parser.add_argument('--pausetimes', dest='pts', default='5.0', help='pause times [5.0]')
-    parser.add_argument('--dimension', dest='exp_dim', default='mobility', help='Parameter to vary (mobility,nodes,...)')
     parser.add_argument('--sessions', dest='sessions', default='2', help='number of sessions (2)')
     parser.add_argument('--specific', dest='specific', default=False, action='store_true')
     #parser.add_argument('--mobility', dest='mobility', default='static', help='mobility model: static,waypoint')
@@ -315,23 +335,22 @@ if __name__ == "__main__" or __name__ == "__builtin__":
     pts=map(lambda x: float(x), args.pts.split(','))
     sessions=int(args.sessions)
     nodes=map(lambda x: int(x), args.nodes.split(','))
+    dimension=map(lambda x: int(x), args.dimension.split(','))
 
-    if args.exp_dim == "mobility" and len(nodes) > 1:
-        raise Exception("Evaluating mobility dimension but more than one nodes value supplied.")
-    elif args.exp_dim == "nodes" and len(pts) > 1:
-        raise Exception("Evaluating nodes dimension but more than one pausetimes value supplied.")
-    elif not args.exp_dim in ["mobility", "nodes"]: 
-        raise Exception("Unknown dimension: %s"%args.exp_dim)
+    variables = { "mobility" : pts, "nodes" : nodes, "dimension" : dimension }
+
+    if len(filter(lambda x: x > 1, map(len, variables.values()))) > 1:
+        raise Exception("Multiple parameters being varied at the same time: %s"%str(variables))
 
     params = {} 
     if args.model: params['model']=args.model
-    params['exp-dim']=args.exp_dim
     params['net-routing']=args.routing
     params['specific']=args.specific
     params['preserve']=args.preserve
     params['h']=int(args.h)
-    params['x']=int(args.x)
-    params['y']=int(args.y)
+    if len(dimension) == 1:
+        params['x']=args.dimension
+        params['y']=args.dimension
     params['query']=args.query
     params['saveconfig']=args.saveconfig
     params['constraints']=args.constraints
@@ -361,5 +380,6 @@ if __name__ == "__main__" or __name__ == "__builtin__":
     if args.worker_predelay: params['worker_predelay'] = args.worker_predelay
     if args.refresh_ms: params['refresh_ms'] = args.refresh_ms
 
-    main(ks,pts,nodes,sessions,params,plot_time_str=args.plot_time_str)
+    #main(ks,pts,nodes,sessions,params,plot_time_str=args.plot_time_str)
+    main(ks,variables,sessions,params,plot_time_str=args.plot_time_str)
 
