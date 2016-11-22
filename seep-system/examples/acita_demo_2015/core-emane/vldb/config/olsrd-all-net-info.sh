84c3c5109416d22d1cc447d8cc9d942fd8816dc5
dokeeffe <d.okeeffe@imperial.ac.uk>
2016-11-22 10:49:24 +0000

    Add script to get net info.

---
+++ seep-system/examples/acita_demo_2015/core-emane/vldb/config/olsrd-all-net-info.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#/sbin/route | grep '^n' | tr -s ' ' | cut -d' ' -f1,5 | grep -P ' \d+$' | sed "s/^/`hostname` /";
+
+#echo "/all" | nc localhost 2006 | grep 'eth0' | tr -s '	' ' '| cut -d$' ' -f1,4 | sed 's/\/[0-9]\+//g' | sed "s/^/`hostname` /"
+echo "/all" | nc localhost 2006 
