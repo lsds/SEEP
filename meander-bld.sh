8bfbf7e009b596b1999dbd44dccf57fbe7a7c285
dokeeffe <d.okeeffe@imperial.ac.uk>
2017-02-17 14:10:56 +0000

    Build standard seep example too.

--- meander-bld.sh
+++ meander-bld.sh
@@ -1,1 +1,10 @@
-mvn clean compile assembly:single ; mkdir -p seep-system/examples/acita_demo_2015/lib ; cp seep-system/target/seep-system-0.0.1-SNAPSHOT.jar seep-system/examples/acita_demo_2015/lib ; pushd seep-system/examples/acita_demo_2015 ; ant ; popd
+mvn clean compile assembly:single 
+
+mkdir -p seep-system/examples/acita_demo_2015/lib 
+cp seep-system/target/seep-system-0.0.1-SNAPSHOT.jar seep-system/examples/acita_demo_2015/lib 
+
+mkdir -p seep-system/examples/stateless-simple-query/lib 
+cp seep-system/target/seep-system-0.0.1-SNAPSHOT.jar seep-system/examples/acita_demo_2015/lib 
+
+pushd seep-system/examples/acita_demo_2015 ; ant ; popd 
+pushd seep-system/examples/stateless-simple-query ; ant ; popd
