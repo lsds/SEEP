212b66cf6e025d8a9f5d636e1192083cac192f08
Dan O'Keeffe <danokeeffe@gmail.com>
2017-06-27 18:01:48 +0000

    Try with k=4 fr.

--- seep-system/examples/acita_demo_2015/core-emane/run-pi-master.sh
+++ seep-system/examples/acita_demo_2015/core-emane/run-pi-master.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 rm ../tmp/*
-cp static/pi_srcsink_constraints.txt ../tmp/mappingRecordIn.txt
+#cp static/pi_srcsink_constraints.txt ../tmp/mappingRecordIn.txt
+cp static/pi_k4fr_constraints.txt ../tmp/mappingRecordIn.txt
 cd ../tmp
 java -classpath "../lib/*" uk.ac.imperial.lsds.seep.Main Master `pwd`/../dist/acita_demo_2015.jar Base
