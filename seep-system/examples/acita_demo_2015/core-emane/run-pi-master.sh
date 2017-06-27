bee9ad8aabf9076cc0a2636ffb12cb265d1c07da
Dan O'Keeffe <danokeeffe@gmail.com>
2017-06-27 12:49:41 +0000

    Switch to fr.

--- seep-system/examples/acita_demo_2015/core-emane/run-pi-master.sh
+++ seep-system/examples/acita_demo_2015/core-emane/run-pi-master.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+cp static/pi_srcsink_constraints.txt ../tmp/mappingRecordIn.txt
 cd ../tmp
 java -classpath "../lib/*" uk.ac.imperial.lsds.seep.Main Master `pwd`/../dist/acita_demo_2015.jar Base
