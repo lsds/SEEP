85d93441712654335cb9313ea2b316d09c7caa64
Dan O'Keeffe <danokeeffe@gmail.com>
2017-06-27 14:44:15 +0000

    Switch back to fdr for now.

--- seep-system/examples/acita_demo_2015/core-emane/run-pi-master.sh
+++ seep-system/examples/acita_demo_2015/core-emane/run-pi-master.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+rm ../tmp/*
 cp static/pi_srcsink_constraints.txt ../tmp/mappingRecordIn.txt
 cd ../tmp
 java -classpath "../lib/*" uk.ac.imperial.lsds.seep.Main Master `pwd`/../dist/acita_demo_2015.jar Base
