d952b00304bad9e75678a2b0e3781da4f6c31387
dokeeffe <d.okeeffe@imperial.ac.uk>
2017-06-30 17:26:55 +0100

    Add gimp script for resizing images.

---
+++ seep-system/examples/acita_demo_2015/core-emane/vldb/config/resize-images.scm
@@ -0,0 +1,10 @@
+; To run try e.g. gimp -i -b '(batch-resize-rename "*.JPG" 604 453)' -b '(gimp-quit 0)'
+(define (batch-resize-rename pattern width height)
+ (let* ((filelist (cadr (file-glob pattern 1))))
+  (while (not (null? filelist))
+   (let* ((filename (car filelist)) (image (car (gimp-file-load RUN-NONINTERACTIVE filename filename))) (drawable (car (gimp-image-get-active-layer image))))
+    (gimp-image-scale-full image width height INTERPOLATION-CUBIC)
+    (let ((nfilename (string-append "resized-" filename)))
+     (gimp-file-save RUN-NONINTERACTIVE image drawable nfilename nfilename))
+    (gimp-image-delete image))
+   (set! filelist (cdr filelist)))))
