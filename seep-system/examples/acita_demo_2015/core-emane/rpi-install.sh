aa1d45a5a49af928b06f1c5b2cd138bb2c2909a8
Dan O'Keeffe <d.okeeffe@imperial.ac.uk>
2017-07-18 07:28:39 +0000

    Update install instructions; Use docpi1 constraints for now.

--- seep-system/examples/acita_demo_2015/core-emane/rpi-install.sh
+++ seep-system/examples/acita_demo_2015/core-emane/rpi-install.sh
@@ -2,25 +2,7 @@
 set -o errexit ; set -o nounset
 sudo apt-get update
 
-sudo apt-get -y install oracle-java8-jdk
-
-#N.B. You need to install Oracle Java 1.7 manually first!
-#TODO: Automate java install
-#if [ ! -f jdk-7u79-linux-x64.tar.gz ]; then
-#    echo "JDK not found"
-#    tar -xzvf jdk-7u79-linux-x64.tar.gz 
-#    sudo cp -r jdk1.7.0_79 /usr/lib/jvm
-#    sudo update-alternatives --install "/usr/bin/java" "java" "/usr/lib/jvm/jdk1.7.0_79/bin/java" 1
-#    sudo update-alternatives --install "/usr/bin/javac" "javac" "/usr/lib/jvm/jdk1.7.0_79/bin/javac" 1
-#    sudo update-alternatives --install "/usr/bin/jar" "jar" "/usr/lib/jvm/jdk1.7.0_79/bin/jar" 1
-## then manually do:
-##    sudo update-alternatives --config java
-##  and select the oracle jdk from the list.
-#    echo "Installed JDK, manual configuration of alternatives required, exiting."
-#    exit 1
-#fi
-
-sudo apt-get -y install maven
+sudo apt-get -y install oracle-java8-jdk maven
 
 mkdir -p install
 cd install
@@ -52,9 +34,12 @@ sudo cp /etc/olsrd/olsrd.conf /etc/olsrd/olsrd.conf.orig
 sudo cp ../vldb/config/olsrd.conf.default.full.txt /etc/olsrd/olsrd.conf
 
 echo "Installing pip + python packages."
-#sudo apt-get -y install python-pip
-#sudo apt-get -y install python-dev
+
+sudo apt-get -y install python-pip
+sudo apt-get -y install python-dev libffi-dev libssl-dev 
 #sudo apt-get -y install libpng12-dev libfreetype6-dev pkg-config
+
+sudo pip install fabric
 
 #python pandas
 #sudo pip install pandas
