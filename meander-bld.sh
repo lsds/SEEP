9b7418969ea42e3c80f02ba94f2b3e74ebd65739
Dan O'Keeffe <danokeeffe@gmail.com>
2017-11-07 14:17:09 +0000

    Fix build script string equality test.

--- meander-bld.sh
+++ meander-bld.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 if [ $# -eq 0 ]
 then
 	echo "No arguments supplied: Do you want to build for \"core\" or \"pi\" experiments?"
@@ -11,7 +13,7 @@ mkdir -p seep-system/examples/acita_demo_2015/lib
 mkdir -p seep-system/examples/stateless-simple-query/lib 
 rm -f seep-system/examples/acita_demo_2015/lib/*.jar
 
-if [ "$1" -eq "core" ]
+if [[ "$1" = "core" ]]
 then
 	cp seep-system/pom.xml.core seep-system/pom.xml
 else
@@ -26,7 +28,7 @@ cp seep-system/target/seep-system-0.0.1-SNAPSHOT.jar seep-system/examples/statel
 
 mkdir -p seep-system/examples/acita_demo_2015/tmp
 
-if [ "$1" -eq "core" ]
+if [[ "$1" = "core" ]]
 then
 	pushd seep-system/examples/acita_demo_2015 ; ant dist-res ; popd 
 else
