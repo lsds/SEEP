2370022778a7de19ab4c266546fa3f3fba84a581
Dan O'Keeffe <danokeeffe@gmail.com>
2017-11-28 22:37:25 +0000

    Only trim explicit directories.

--- seep-system/examples/acita_demo_2015/core-emane/trim-logs.sh
+++ seep-system/examples/acita_demo_2015/core-emane/trim-logs.sh
@@ -1,1 +1,8 @@
-for f in `find log/$1 -type f -name '*worker*.log' | xargs grep -H -c 'FINISHED' | grep 0$ | cut -d':' -f1`; do sudo rm $f ; done
+#!/bin/bash
+if [ "$1" == "" ] ; then
+	echo "No experiment directory given"
+	exit
+fi
+
+
+for f in `find $1 -type f -name '*worker*.log' | xargs grep -H -c 'FINISHED' | grep 0$ | cut -d':' -f1`; do sudo rm $f ; done
